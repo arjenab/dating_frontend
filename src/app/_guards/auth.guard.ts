36459311b2b55941209a3bbc4ce3b7dc429f0d88
Arjen <Arjen.ab@hybrit.org>
2019-11-22 14:22:29 +0100

    user profile editing

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
     private authService: AuthService,
     private router: Router,
     private alertify: AlertifyService
-    ) {};
+    ) {}
   canActivate(): boolean  {
     if (this.authService.loggedIn()) {
       return true;
@@ -21,5 +21,4 @@ export class AuthGuard implements CanActivate {
     this.router.navigate(['/home']);
     return false;
   }
-  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,10 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { AuthGuard } from './_guards/auth.guard';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 
 
 export function tokenGetter() {
@@ -44,6 +48,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MessagesComponent,
       MemberCardComponent,
       MemberDetailComponent,
+      MemberEditComponent,
    ],
    imports: [
       BrowserModule,
@@ -66,7 +71,10 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       ErrorInterceptorProvider,
       MemberDetailResolver,
       MemberListResolver,
-      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+      MemberEditResolver,
+      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig },
+      AuthGuard,
+      PreventUnsavedChanges,
    ],
    bootstrap: [
       AppComponent
