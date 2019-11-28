911399baa3bf91de1c981c7f620d1f436745ad22
Arjen <Arjen.ab@hybrit.org>
2019-11-28 09:18:27 +0100

    added date control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
-
+import { TimeAgoPipe } from 'time-ago-pipe';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
@@ -48,6 +48,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       HomeComponent,
       RegisterComponent,
       MemberListComponent,
+      TimeAgoPipe,
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
