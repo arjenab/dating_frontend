d53bdb6244bf8a258972c3a872c026c010ad0352
Arjen <Arjen.ab@hybrit.org>
2019-11-29 14:01:37 +0100

    added filtering

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -8,10 +8,12 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    PageSize = 5;
     constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.PageSize).pipe(
             catchError(error => {
                 this.alertify.error('problem retrieving members');
                 this.router.navigate(['/home']);
--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { AlertifyService } from '../../_services/alertify.service';
 import { User } from '../../_models/User';
 import { UserService } from '../../_services/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResults } from 'src/app/_models/Pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -11,13 +12,47 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {};
+  pagination: Pagination;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private userService: UserService, private alertyfy: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data.users;
+      this.users = data.users.results;
+      this.pagination = data.users.pagination;
     });
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+      .subscribe(
+        (res: PaginatedResults<User[]>) => {
+          this.users = res.results;
+          this.pagination = res.pagination;
+        }, err => {
+          this.alertyfy.error(err);
+        }
+      );
   }
 
 }
